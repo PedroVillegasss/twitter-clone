4d6cd10fcc17dfacef5f764ec7914f87cd288637
PedroVillegasss <ppvillegas@miuandes.cl>
2024-05-27 14:44:57 -0400

    feat: allow user registration. Users that are not logged in cannot create, edit or delete a post

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show ; end
+
+  def create
+    user = User.new user_params
+    if user.save
+        redirect_to users_path
+    else
+        render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+        redirect_to users_path
+    else
+        render :edit
+    end
+  end
+
+  private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+
+    def find_user
+        @user = User.find params['id']
+    end
+end
